dc331f33849c476283fc1238efde2107b1b55b91
FaaizHaikal <faaizhaikal@gmail.com>
2025-03-01 16:01:52 +0700

    feat: saving schema and dao

---
+++ app/src/main/java/com/example/spendiary/data/local/dao/SavingDao.kt
@@ -0,0 +1,27 @@
+package com.example.spendiary.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.spendiary.data.local.entities.Saving
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SavingDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSaving(saving: Saving)
+
+    @Update
+    suspend fun updateSaving(saving: Saving)
+
+    @Delete
+    suspend fun deleteSaving(saving: Saving)
+
+    @Query("SELECT * FROM Savings ORDER BY date DESC")
+    fun getAllSavings(): Flow<List<Saving>>
+
+    // TODO: Query to get current year saving
+}
---
+++ app/src/main/java/com/example/spendiary/data/local/entities/Saving.kt
@@ -0,0 +1,13 @@
+package com.example.spendiary.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Date
+
+@Entity(tableName = "Savings")
+data class Saving(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val date: Date,
+    val amount: Double
+)
