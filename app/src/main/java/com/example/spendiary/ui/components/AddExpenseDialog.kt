f3347467023d249956dd346b74565fa8ac81739f
FaaizHaikal <faaizhaikal@gmail.com>
2025-03-02 23:29:59 +0700

    feat: stylize with colors

--- app/src/main/java/com/example/spendiary/ui/components/AddExpenseDialog.kt
+++ app/src/main/java/com/example/spendiary/ui/components/AddExpenseDialog.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -18,8 +21,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.example.spendiary.ui.theme.PurpleAccent2
+import com.example.spendiary.ui.theme.White60
+import com.example.spendiary.ui.theme.White87
 import com.example.spendiary.utils.CurrencyVisualTransformation
 import com.example.spendiary.utils.DecimalFormatter
 import java.time.LocalDate
@@ -44,7 +51,14 @@ fun AddExpenseDialog(
                     onValueChange = { description = it },
                     label = { Text("Description") },
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                    singleLine = true
+                    singleLine = true,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = White87,
+                        focusedLabelColor = White87,
+                        unfocusedLabelColor = White60,
+                        unfocusedBorderColor = White60,
+                        cursorColor = White87,
+                    )
                 )
 
                 Spacer(modifier = Modifier.height(8.dp))
@@ -56,7 +70,14 @@ fun AddExpenseDialog(
                     label = { Text("Amount") },
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
                     visualTransformation = CurrencyVisualTransformation(decimalFormatter),
-                    singleLine = true
+                    singleLine = true,
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = White87,
+                        focusedLabelColor = White87,
+                        unfocusedLabelColor = White60,
+                        unfocusedBorderColor = White60,
+                        cursorColor = White87,
+                    )
                 )
 
                 Spacer(modifier = Modifier.height(8.dp))
@@ -73,13 +94,23 @@ fun AddExpenseDialog(
                         println("Saving Expense: $description - Rp. $amount")
                         onDismiss()
                     }
-                }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = PurpleAccent2,
+                    contentColor = White87
+                ),
             ) {
                 Text("Save")
             }
         },
         dismissButton = {
-            TextButton(onClick = { onDismiss() }) {
+            TextButton(
+                onClick = { onDismiss() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = White87
+                ),
+            ) {
                 Text("Cancel")
             }
         }
