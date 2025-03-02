1e878d209970c957a6bb322edfe7192124c315b1
FaaizHaikal <faaizhaikal@gmail.com>
2025-03-02 23:30:05 +0700

    feat: stylize with colors

--- app/src/main/java/com/example/spendiary/ui/components/DatePicker.kt
+++ app/src/main/java/com/example/spendiary/ui/components/DatePicker.kt
@@ -2,11 +2,18 @@ package com.example.spendiary.ui.components
 
 import android.app.DatePickerDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.spendiary.ui.theme.DarkBackground
+import com.example.spendiary.ui.theme.PurpleAccent
+import com.example.spendiary.ui.theme.White87
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
 
 @Composable
 fun DatePicker(selectedDate: LocalDate, onDateSelected: (LocalDate) -> Unit) {
@@ -21,7 +28,16 @@ fun DatePicker(selectedDate: LocalDate, onDateSelected: (LocalDate) -> Unit) {
         selectedDate.dayOfMonth
     )
 
-    Button(onClick = { datePickerDialog.show() }) {
-        Text("Date: ${selectedDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))}")
+    Button(
+        onClick = { datePickerDialog.show() },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = DarkBackground,
+            contentColor = White87
+        )
+    ) {
+        Text(
+            text = "Date: ${selectedDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))}",
+            fontSize = 12.sp
+        )
     }
 }
